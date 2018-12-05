4cf4fe275db9dce03c8c2efe7b95ae94d323130b
Alonso <alonso2022@163.com>
2018-12-05 16:08:25 +0800

    gh-pages

---
+++ js/chunk-2d22a0ea.161e89a3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22a0ea"],{dfa3:function(e,n,r){"use strict";r.r(n);r("a481");var t,a,u={beforeCreate:function(){var e=this.$route,n=e.params,r=e.query,t=n.path;this.$router.replace({path:"/"+t,query:r})},render:function(e){return e()}},o=u,i=r("2877"),p=Object(i["a"])(o,t,a,!1,null,null,null);p.options.__file="index.vue";n["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d22a0ea.161e89a3.js.map
